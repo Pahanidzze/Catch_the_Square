69a36941b1d83514ac2a9a7fff82c31e636b7e99
Pahanidzze <0pahan001@gmail.com>
2023-08-14 21:05:57 +0500

    cleaning using

--- Squares/AllySquare.cs
+++ Squares/AllySquare.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SFML.Audio;
-using SFML.Graphics;
+﻿using SFML.Graphics;
 using SFML.System;
 
 namespace Catch_the_Square.Squares
